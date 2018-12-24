739a58a2c5ed2edddb33918b4660adcfb076ab2d
chen841115 <chen841115@gmail.com>
2018-12-24 19:00:01 +0800

    chen's 3

--- HW3_net.py
+++ HW3_net.py
@@ -7,25 +7,27 @@ from mininet.cli import CLI
 def MininetTopo():
     net = Mininet()
     info("Create host nodes.\n")
-    h1 = net.addHost("h1" , ip='192.168.1.11')
-    h2 = net.addHost("h2" , ip='192.168.1.12')
-    h3 = net.addHost("h3" , ip='192.168.2.13')
-    #h4 = net.addHost("h4" , ip='192.168.3.13')
+    h1 = net.addHost("h1" , ip='192.168.1.11' , mac='00:00:00:00:00:01')
+    h2 = net.addHost("h2" , ip='192.168.1.12' , mac='00:00:00:00:00:02')
+    h3 = net.addHost("h3" , ip='192.168.2.13' , mac='00:00:00:00:00:03')
+    h4 = net.addHost("h4" , ip='192.168.3.13' , mac='00:00:00:00:00:04')
  
  
  
     info("Create switch node.\n")
     #s1 = net.addSwitch("s1",failMode = 'standalone')
-    s1 = net.addSwitch("s1",failMode = 'secure',protocols = 'OpenFlow15')
-    s2 = net.addSwitch("s2",failMode = 'secure',protocols = 'OpenFlow15')
+    s1 = net.addSwitch("s1",failMode = 'secure',protocols = 'OpenFlow13' , mac='10:00:00:00:00:01')
+    s2 = net.addSwitch("s2",failMode = 'secure',protocols = 'OpenFlow13' , mac='10:00:00:00:00:02')
+    s3 = net.addSwitch("s3",failMode = 'secure',protocols = 'OpenFlow13' , mac='10:00:00:00:00:03')
  
     info("Create Links. \n")
     net.addLink(h1,s1)
     net.addLink(h2,s1)
     net.addLink(h3,s2)
-    #net.addLink(h4,s1)
+    net.addLink(h4,s3)
     #net.addLink(h4,s2)
     net.addLink(s1,s2)
+    net.addLink(s2,s3)
 
     info("Create controller ot switch. \n")
     net.addController(controller=RemoteController,ip='10.0.2.15',port=6633)
--- HW3_ryu.py
+++ HW3_ryu.py
@@ -3,16 +3,16 @@
 # 2016.07.30 kshuang
  
 from ryu.base import app_manager
-from ryu.ofproto import ofproto_v1_5
+from ryu.ofproto import ofproto_v1_3
 from ryu.controller.handler import set_ev_cls
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller import ofp_event
-from ryu.ofproto import ofproto_v1_5_parser
+from ryu.ofproto import ofproto_v1_3_parser
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
  
 class MyRyu(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_5.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     normal_port = []
  
     def __init__(self, *args, **kwargs):
@@ -37,8 +37,10 @@ class MyRyu(app_manager.RyuApp):
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
- 
+        print datapath.id
         for stat in ev.msg.body:
+            print stat.port_no
+            print ofproto.OFPP_MAX
             if stat.port_no < ofproto.OFPP_MAX:
                 self.normal_port.append(stat.port_no)
         
@@ -46,54 +48,75 @@ class MyRyu(app_manager.RyuApp):
             print self.normal_port
             # h3 to s2
             match = parser.OFPMatch(in_port=self.normal_port[0])
-            actions = [parser.OFPActionOutput(self.normal_port[1])]
-            self.add_flow(datapath, 1, match, actions)
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+            self.add_flow(datapath, 1, match, actions,0)
             # s2 to h3
             match = parser.OFPMatch(in_port=self.normal_port[1])
-            actions = [parser.OFPActionOutput(self.normal_port[0])]
-            self.add_flow(datapath, 1, match, actions)
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+            self.add_flow(datapath, 1, match, actions,0)
+            # s2 to h3
+            match = parser.OFPMatch(in_port=3)
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+            self.add_flow(datapath, 1, match, actions,0)
         if len(self.normal_port) == 3:
             print self.normal_port
             
             # h1 to all
             match = parser.OFPMatch(in_port=self.normal_port[0])
             actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-            self.add_flow(datapath, 1, match, actions)
+            self.add_flow(datapath, 1, match, actions,0)
             # h2 to all
             match = parser.OFPMatch(in_port=self.normal_port[1])
             actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-            self.add_flow(datapath, 1, match, actions)
+            self.add_flow(datapath, 1, match, actions,0)
             # s2 to all
             match = parser.OFPMatch(in_port=self.normal_port[2])
             actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
-            self.add_flow(datapath, 1, match, actions)
+            self.add_flow(datapath, 1, match, actions,0)
+            # s2 to h3
+            match = parser.OFPMatch(in_port=4)
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+            self.add_flow(datapath, 1, match, actions,0)
         # clear port record after add flow entry
         self.normal_port = []
  
-    def add_flow(self, datapath, priority, match, actions):
+    def add_flow(self, datapath, priority, match, actions ,table_id):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
         #print datapath.id
         #table_id = 1
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions)]
- 
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, command=ofproto.OFPFC_ADD, match=match, instructions=inst)
+        next_table_id = 1
+        inst = [parser.OFPInstructionGotoTable(next_table_id)]
+        #inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions)]
+    
+        mod = parser.OFPFlowMod(table_id = table_id, datapath=datapath, priority=priority, command=ofproto.OFPFC_ADD, match=match, instructions=inst)
         datapath.send_msg(mod)
-        print mod
+        #print mod
         #---------------------------------------------------------------在這以下註解掉就是普通的可以互ping的情形
-        #---------------------------------------------------------------我這裡加上第二個條件就是從port1來的 都接到port2去 所以從port1來的不會到port3 
-        #table_id = 2
-        match = parser.OFPMatch(in_port=1)#, eth_dst='ff:ff:ff:ff:ff:ff'
-        actions = [parser.OFPActionOutput(2)]
+        #---------------------------------------------------------------我這裡加上第二個條件就是從port1來的 都接到port2去 所以從port1來的不會到port3
+        #""" 
+        table_id = 1
+        #match = parser.OFPMatch(eth_dst='00:00:00:00:00:01')
+        #actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+        match = parser.OFPMatch()#, eth_dst='ff:ff:ff:ff:ff:ff'
+        #actions = [parser.OFPActionOutput(2)]
+        print actions
         #print match
         #print actions
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions)]
  
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority+1, command=ofproto.OFPFC_ADD, match=match, instructions=inst)
+        mod = parser.OFPFlowMod(table_id = table_id, datapath=datapath, priority=priority+1, command=ofproto.OFPFC_ADD, match=match, instructions=inst)
         datapath.send_msg(mod)
-        print mod
-        
+
+        match = parser.OFPMatch(eth_dst='00:00:00:00:00:03')
+        actions = [parser.OFPActionOutput(1)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,actions)]
+ 
+        mod = parser.OFPFlowMod(table_id = table_id, datapath=datapath, priority=priority+2, command=ofproto.OFPFC_ADD, match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+        #print mod
+        #"""
     """
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
