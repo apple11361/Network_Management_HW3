22c2615d55ea25e2fbd807edd1ffa8e59eee9798
apple11361 <apple113611361@gmail.com>
2018-12-25 14:46:29 +0800

    Bugfix: merge the network setup files
    Merge the network setup files.
    
    Add a switch to our network environment, totally three switches.

--- network_setup.py
+++ network_setup.py
@@ -21,12 +21,15 @@ class MyTopo( Topo ):
         Host3 = self.addHost( 'h3', ip='192.168.2.11/24', mac='00:00:00:00:00:03' )
         Switch1 = self.addSwitch( 's1', protocols="OpenFlow13", mac='10:00:00:00:00:01' )
         Switch2 = self.addSwitch( 's2', protocols="OpenFlow13", mac='10:00:00:00:00:02' )
+        Switch3 = self.addSwitch( 's3', protocols="OpenFlow13", mac='10:00:00:00:00:03' )
 
         # Add links
         self.addLink( Host1, Switch1 )
         self.addLink( Host2, Switch1 )
         self.addLink( Host3, Switch2 )
         self.addLink( Switch1, Switch2 )
+        self.addLink( Switch1, Switch3 )
+        self.addLink( Switch2, Switch3 )
 
 #topos = { 'mytopo': ( lambda: MyTopo() ) }
 
